5c5209061a2bc3cca59d5c1dacb18108d4c2f255
Alen Ordog <alen_ordog@epam.com>
2024-07-02 15:31:24 +0200

    Create custom pipes

--- src/app/shared/pipes/custom-date.pipe.ts
+++ src/app/shared/pipes/custom-date.pipe.ts
@@ -1,8 +1,11 @@
-import { Pipe } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
     name: 'customDate'
 })
-export class CustomDatePipe {
-    // Add your code here
+export class CustomDatePipe implements PipeTransform {
+    transform(date: Date, format: string = 'dd.MM.yyyy') {
+        return new DatePipe('en-US').transform(date, format);
+    }
 }
--- src/app/shared/pipes/duration.pipe.ts
+++ src/app/shared/pipes/duration.pipe.ts
@@ -1,8 +1,23 @@
-import { Pipe } from "@angular/core";
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
     name: 'duration'
 })
-export class DurationPipe {
-    // Add your code here
+export class DurationPipe implements PipeTransform {
+    transform(value: number): string {
+        if (!value && value !== 0) {
+          return '';
+        }
+    
+        const hours = Math.floor(value / 60);
+        const minutes = value % 60;
+    
+        const paddedHours = hours.toString().padStart(2, '0');
+        const paddedMinutes = minutes.toString().padStart(2, '0');
+    
+        const hourLabel = hours === 1 ? 'hour' : 'hours';
+    
+        return `${paddedHours}:${paddedMinutes} ${hourLabel}`;
+      }
+    
 }
