7f233948fd0524055a23e56ff1102ae0454a4a35
Alen Ordog <alen_ordog@epam.com>
2024-07-02 15:30:44 +0200

    add registration form without auth logic

--- src/app/shared/components/registration-form/registration-form.component.ts
+++ src/app/shared/components/registration-form/registration-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { EmailValidator, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { emailValidator } from '@app/shared/directives/email.directive';
 
 @Component({
   selector: 'app-registration-form',
@@ -8,5 +9,30 @@ import { FormGroup } from '@angular/forms';
 })
 export class RegistrationFormComponent {
   registrationForm!: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.registrationForm = fb.group({
+      name: ['', [
+        Validators.required,
+        Validators.minLength(6),
+      ]],
+      email: ['', [
+        Validators.required,
+        emailValidator()
+      ]],
+      password: ['', [
+        Validators.required
+      ]]
+    });
+ }
+ onFormSubmit() {
+  if (this.registrationForm.valid) {
+    console.log(this.registrationForm.value);
+    this.registrationForm.reset();
+    // TODO: Implement authentication logic here
+  } else {
+    alert('Form is not valid');
+    this.registrationForm.markAllAsTouched(); 
+  }  }
+  
   // Use the names `name`, `email`, `password` for the form controls.
 }
