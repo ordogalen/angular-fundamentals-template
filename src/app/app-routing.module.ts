2f76674d5ffdcc86e8df542db93904c6ef24888b
Alen Ordog <alen_ordog@epam.com>
2024-07-08 09:10:04 +0200

    App routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,24 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CourseFormComponent, LoginFormComponent, RegistrationFormComponent } from './shared/components';
+import { CoursesComponent } from './features/courses/courses.component';
+import { CourseInfoComponent } from './features/course-info/course-info.component';
+import { NotAuthorizedGuard } from './auth/guards/not-authorized.guard';
+import { AuthorizedGuard } from './auth/guards/authorized.guard';
+import { AdminGuard } from './user/guards/admin.guard';
 
-export const routes: Routes = [
-    /* Add your code here */
+const routes: Routes = [
+    {path: "login", component: LoginFormComponent, canActivate: [NotAuthorizedGuard]},
+    {path: "register", component: RegistrationFormComponent, canActivate: [NotAuthorizedGuard]},
+    {path: "courses", component: CoursesComponent, canActivate: [AuthorizedGuard] },
+    {path: "", component: CoursesComponent, canActivate: [AuthorizedGuard] },
+    {path: "courses/add", component: CourseFormComponent, canActivate: [AdminGuard] },
+    {path: "courses/:id", component: CourseInfoComponent, canActivate: [AuthorizedGuard] },
+    {path: "courses/edit/:id", component: CourseFormComponent, canActivate: [AdminGuard] },
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
